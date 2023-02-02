d64bbbacb93e36a3e0ae53bb7ff89fa46e20c6f9
ahmed boulaouane <bouahmed530@gmail.com>
2023-02-02 15:13:53 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+//import './App.css';
+import Todolist from "./component/Todolist"
+import './component/App.css';
+import j from './component/qq.gif'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
